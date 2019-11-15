8ba738ffd2bc1b8c22a5b10f6765b43ce588a039
Jonas Laursen <jona8690@easv365.dk>
2019-11-15 10:59:38 +0100

    Ex 1 & 2 from class

--- FSharp-Comp1/Program.fs
+++ FSharp-Comp1/Program.fs
@@ -1,4 +1,19 @@
-﻿ha// Learn more about F# at http://fsharp.org
+﻿// Learn more about F# at http://fsharp.org
+
+// Ex 1
+let rec count aList key = match aList with
+    | [] -> 0
+    | h :: t when h < key -> count t key
+    | h :: t when h = key -> 1 + (count t key)
+    | _ -> 0
+
+
+// Ex 2
+let rec insert item aList = match aList with
+    | [] -> [ item ]
+    | h :: t when item <= h -> [ item ] @ aList
+    | h :: t when item > h -> [ h ] @ insert item t
+    | _ -> failwith "Seek Shelter"
 
 open System
 
